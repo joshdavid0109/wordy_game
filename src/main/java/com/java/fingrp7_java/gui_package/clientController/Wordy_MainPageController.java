d94150d25f856a047c60158d2c17f3d7d6c97c14
MAERVIN <2211353@slu.edu.ph>
2023-04-30 10:59:12 +0800

    Create GUI for Main Page

--- src/main/java/com/java/fingrp7_java/gui_package/clientController/Wordy_MainPageController.java
+++ src/main/java/com/java/fingrp7_java/gui_package/clientController/Wordy_MainPageController.java
@@ -1,4 +1,43 @@
 package com.java.fingrp7_java.gui_package.clientController;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+import java.awt.*;
+
 public class Wordy_MainPageController {
+
+    @FXML
+    protected Text playerName;
+    @FXML
+    protected Text playerStatus;
+    @FXML
+    protected Button playGame;
+    @FXML
+    protected Button topPlayers;
+    @FXML
+    protected Button topLongestWords;
+    @FXML
+    protected Button logout;
+
+    @FXML
+    void playGame(MouseEvent event) {
+        System.out.println("hi");
+    }
+
+    @FXML
+    void showTopPlayers(MouseEvent event) {
+
+    }
+
+    @FXML
+    void showTopLongestWords(MouseEvent event) {
+
+    }
+
+    @FXML
+    void logout(MouseEvent event) {
+
+    }
 }
