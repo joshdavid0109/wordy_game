1ead9830fd80b3791ccfc72728ed50f3d4cd8f14
INSTRUCT-D522lab <2211353@slu.edu.ph>
2023-05-02 08:44:36 +0800

    Show ACHIEVEMENT AND TOP PLAYERS

--- src/main/java/com/java/fingrp7_java/gui_package/clientController/Wordy_AchievementController.java
+++ src/main/java/com/java/fingrp7_java/gui_package/clientController/Wordy_AchievementController.java
@@ -1,4 +1,36 @@
 package com.java.fingrp7_java.gui_package.clientController;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Wordy_AchievementController {
+
+    @FXML
+    private Button mainPage;
+
+    @FXML
+    void toMainPage(ActionEvent event) {
+        FXMLLoader showLongestWord = new FXMLLoader();
+
+        showLongestWord.setLocation(getClass().getResource("/com/java/fmxl/mainPage.fxml"));
+
+        Parent root = null;
+        try {
+            root = showLongestWord.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) mainPage.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
